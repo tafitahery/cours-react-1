521dd4f79bf930b974006c72baaed9d5b6f29168
Tafita Hery <tafitahery@gmail.com>
2022-06-29 09:48:27 +0300

    Createion des pages Home et About

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond a rien de declarer au dessus */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
