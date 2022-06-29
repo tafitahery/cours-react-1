5229c0a3f5f812821cce69e0cca3f6671b1e1cfc
Tafita Hery <tafitahery@gmail.com>
2022-06-29 16:25:18 +0300

    Création du composant Countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+
+  // Le useEffect se joue lorsque le composant est monté
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(({ data }) => setData(data));
+  }, []);
+
   return (
-    <div>
+    <div className="countries">
       <h1>COUNTRIES</h1>
+      <ul>
+        {data.map((country, index) => (
+          <li key={index}>{country.name.common}</li>
+        ))}
+      </ul>
     </div>
   );
 };
