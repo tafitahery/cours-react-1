2d7318d3cc16794d11c80684dee72d686690d091
Tafita Hery <tafitahery@gmail.com>
2022-06-29 15:52:06 +0300

    Création du composant Logo

---
+++ src/components/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+  return (
+    <div>
+      <h1>COUNTRIES</h1>
+    </div>
+  );
+};
+
+export default Countries;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Countries from '../components/Countries';
+import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const Home = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
-      <h1>ACCEUIL</h1>
+      <Countries />
     </div>
   );
 };
